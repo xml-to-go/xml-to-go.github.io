f9f23173bc724e7ae913477559157e80650f678e
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2022-08-22 00:50:31 +0300

    Use https://github.com/miku/zek

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/xml-to-go/xml-to-go.github.io
 
 go 1.18
+
+require github.com/miku/zek v0.1.16
+
+require (
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"go/format"
+	"strings"
 	"syscall/js"
+
+	"github.com/miku/zek"
 )
 
 func main() {
@@ -14,5 +19,32 @@ func main() {
 }
 
 func xmlDataToGoTypeCode(this js.Value, args []js.Value) interface{} {
-	return args[0].String()
+	var (
+		buffer   = new(bytes.Buffer)
+		rootNode = new(zek.Node)
+		sw       = zek.NewStructWriter(buffer)
+	)
+
+	_, err := rootNode.ReadFrom(strings.NewReader(args[0].String()))
+	if err != nil {
+		fmt.Printf("Cannot read Node from source XML with err: %s\n", err)
+
+		return ""
+	}
+
+	err = sw.WriteNode(rootNode)
+	if err != nil {
+		fmt.Printf("Cannot write Node from source XML with err: %s\n", err)
+
+		return ""
+	}
+
+	source, err := format.Source(buffer.Bytes())
+	if err != nil {
+		fmt.Printf("Format source code with err: %s\n", err)
+
+		return ""
+	}
+
+	return string(source)
 }
